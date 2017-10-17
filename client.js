4683f68a15ae22af7229d33cd4f348dbb2f38848
Parin Chiamananthapong <apemon642002@hotmail.com>
2017-10-17 07:54:09 +0700

    delete unused file in new version

--- client.js
+++ client.js
@@ -1,24 +1,22 @@
-var Q = require('q');
-var os = require('os');
 var fs = require('fs');
 var argv = require('argv');
-var request = require('request');
+var os = require('os');
+var Q = require('q');
+var moment = require('moment');
+// cryptocompare api
+global.fetch = require('node-fetch');
+var cryptocompare = require('cryptocompare');
 
 // prase param
 var args = argv.option([
     {
-        name: "url",
+        name: "fromsymbol",
         type: "string"
     },{
-        name: "symbol",
+        name: "tosymbol",
         type: "string"
     }
 ]).run();
-
-// initial parameter
-var path = args.options.url;
-var symbol = args.options.symbol;
-symbol = symbol.toUpperCase();
 
 // string replace all
 String.prototype.replaceAll = function(search, replacement) {
@@ -26,9 +24,33 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-// call ws
-request.get("http://crypto-data-fetcher.azurewebsites.net/price/" + symbol, function(err, res, body){
-    // write file
-    var content = body.replaceAll("<br>",os.EOL);
-    fs.writeFileSync(symbol + "USD.txt",content);
+// initial parameter
+var fromsymbol = args.options.fromsymbol.toUpperCase();
+var tosymbol = args.options.tosymbol.toUpperCase();
+
+// get historical data
+getHistoricalData(fromsymbol, tosymbol)
+.then(datas => {
+  var ticket = fromsymbol + tosymbol;
+  var result = "";
+  for(i = 0;i < datas.length; i++){ 
+    var data = datas[i];
+    var date = moment(data.time * 1000).format("YYYY-MM-DD");
+    result += ticket + "," + date + "," + data.open + "," + data.high + "," + data.low + "," + data.close + os.EOL;
+  }
+  // write file content
+  fs.writeFileSync(ticket + ".txt",result);
+}).catch(error => {
+    fs.writeFileSync(ticket + "-error.txt",error);
 });
+
+function getHistoricalData(fromsymbol, tosymbol) {
+    var deferred = Q.defer();
+    cryptocompare.histoDay(fromsymbol,tosymbol,{limit:'none'})
+      .then(datas => {
+        deferred.resolve(datas);
+      }).catch(error => {
+        deferred.reject(error);
+      });
+    return deferred.promise;
+  }
